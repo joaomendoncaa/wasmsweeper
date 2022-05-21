442c40062c7a57f995338eaedadc03b71cbb0da3
joaomendoncaa <joaommm0312@gmail.com>
2022-05-21 21:23:16 +0100

    open_field functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,21 @@
 mod random;
 mod sweeper;
 
+use std::cell::RefCell;
+
 use sweeper::Sweeper;
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
+thread_local! {
+    static SWEEPER: RefCell<Sweeper> = RefCell::new(Sweeper::new(10, 10, 5));
 }
 
 #[wasm_bindgen(js_name = getBoardState)]
 pub fn get_board_state() -> String {
-    let sweeper = Sweeper::new(10, 10, 5);
+    return SWEEPER.with(|sweeper| sweeper.borrow().to_string());
+}
 
-    return sweeper.to_string();
+#[wasm_bindgen(js_name = openField)]
+pub fn open_field(x: usize, y: usize) {
+    SWEEPER.with(|sweeper| sweeper.borrow_mut().open((x, y)));
 }
