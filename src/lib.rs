316d3e410f3878c36f7ef85c3d1be8a7df475640
joaomendoncaa <joaommm0312@gmail.com>
2022-05-21 19:05:12 +0100

    toggle_flag display flag test

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,50 @@
 mod random;
 
-use std::collections::HashSet;
+use std::{
+    collections::HashSet,
+    fmt::{Display, Write},
+};
 
 use random::random_range;
 
-type Position = (usize, usize);
+pub type Position = (usize, usize);
+
+pub enum MineOpeningResult {
+    Mine,
+    NoMine(u8),
+}
 
 #[derive(Debug)]
-struct Sweeper {
+pub struct Sweeper {
     width: usize,
     height: usize,
     fields_open: HashSet<Position>,
     fields_flagged: HashSet<Position>,
     mines: HashSet<Position>,
+}
+
+impl Display for Sweeper {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for y in 0..self.height {
+            for x in 0..self.width {
+                let position = (x, y);
+
+                if !self.fields_open.contains(&position) {
+                    f.write_str("⬛ ")?;
+                } else if self.mines.contains(&position) {
+                    f.write_str("💣 ")?;
+                } else if self.fields_flagged.contains(&position) {
+                    f.write_str("🚩 ")?;
+                } else {
+                    write!(f, " {} ", self.get_neightbors_mines(position))?;
+                }
+            }
+
+            f.write_char('\n')?;
+        }
+
+        Ok(())
+    }
 }
 
 impl Sweeper {
@@ -33,16 +65,66 @@ impl Sweeper {
             },
         };
     }
+
+    pub fn get_neighbors_fields_iter(&self, (x, y): Position) -> impl Iterator<Item = Position> {
+        let width = self.width;
+        let height = self.height;
+
+        let iter = x.max(1) - 1..=(x + 1).min(width - 1);
+
+        iter.flat_map(move |i| (y.max(1) - 1..=(y + 1).min(height - 1)).map(move |j| (i, j)))
+            .filter(move |&position| position != (x, y))
+    }
+
+    pub fn get_neightbors_mines(&self, position: Position) -> u8 {
+        self.get_neighbors_fields_iter(position)
+            .filter(|pos| self.mines.contains(pos))
+            .count() as u8
+    }
+
+    pub fn open(&mut self, position: Position) -> Option<MineOpeningResult> {
+        if self.fields_flagged.contains(&position) {
+            return None;
+        }
+
+        self.fields_open.insert(position);
+
+        let is_mine: bool = self.mines.contains(&position);
+
+        if is_mine {
+            return Some(MineOpeningResult::Mine);
+        }
+
+        return Some(MineOpeningResult::NoMine(0));
+    }
+
+    pub fn toggle_flag(&mut self, position: Position) {
+        if self.fields_open.contains(&position) {
+            return;
+        }
+
+        if self.fields_flagged.contains(&position) {
+            self.fields_flagged.remove(&position);
+        }
+
+        self.fields_flagged.insert(position);
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::Sweeper;
+    use crate::{random::random_range, Sweeper};
 
     #[test]
     fn sweeper_instance() {
-        let sweeper = Sweeper::new(10, 10, 5);
+        let mut sweeper = Sweeper::new(10, 10, 2);
 
-        println!("Sweeper Instance data: \n{:?}", sweeper);
+        sweeper.open((random_range(0, 10, true), random_range(0, 10, true)));
+        sweeper.open((random_range(0, 10, true), random_range(0, 10, true)));
+        sweeper.open((random_range(0, 10, true), random_range(0, 10, true)));
+
+        sweeper.toggle_flag((random_range(0, 10, true), random_range(0, 10, true)));
+
+        println!("{}", sweeper);
     }
 }
